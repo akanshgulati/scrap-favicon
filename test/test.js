8e1cdda8061e9b35818e95261b42f3ad2b4760b2
Akansh Gulati <akanshgulati@gmail.com>
2019-07-27 15:04:40 +0530

    test(index.js): update and add test for scrap favicon

--- test/test.js
+++ test/test.js
@@ -6,22 +6,44 @@ test('basic', async function (t) {
     t.is(value.images.length, 1);
 });
 
-test('with redirects', async function (t) {
-    const result = await scrapFavicon('https://www.github.com');
+test('with redirects 1', function (t) {
+    return scrapFavicon('https://akansh.com', {maxRedirect: 1}).then(result =>{
+        t.is(result.redirects.length, 1);
+    });
+});
 
-    t.true(result.redirects.length > 0);
+test('with redirects 3', function (t) {
+    return scrapFavicon('http://akansh.com', {maxRedirect: 2}).then(response => {
+        t.is(response.redirects.length, 2, "with redirects 3 failed");
+    });
+});
+
+test('with redirects 0', function (t) {
+    return scrapFavicon('https://www.akansh.com', {maxRedirect: 0}).then(response => {
+        t.is(response.redirects.length, 0, "with redirects 0 failed");
+    });
 });
 
 test('only urls as configuration', async (t)=>{
-    const result = await scrapFavicon('https://www.akansh.com', {urlsOnly: true});
+    const result = await scrapFavicon('https://www.akansh.com/', {urlsOnly: true});
     t.true(typeof result.images[0].width === 'undefined');
 });
 
-test('max redirects', async t => {
-    try {
-        await scrapFavicon('https://akansh.com', {maxRedirect: 0});
-    } catch(err) {
+test('max redirects error', async function (t) {
+    return scrapFavicon('https://akansh.com', {maxRedirect: 0}).catch((err) => {
+        console.log("Here");
         t.is(err.message, 'Max redirect limit reached');
-    }
-    // t.is(error.message, 'Max redirect limit reached');
+    });
+});
+
+test('http url does not exists', (t)=> {
+    return scrapFavicon('http://a.com').then(()=>{}, (err) => {
+        t.true(!!err);
+    });
+});
+
+test('https url does not exists', (t)=> {
+    return scrapFavicon('http://a.com').then(()=>{}, (err) => {
+        t.true(!!err);
+    });
 });
