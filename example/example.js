417473297673b5addfe9a8b96876b255835df365
Akansh Gulati <akanshgulati@gmail.com>
2019-07-27 15:04:19 +0530

    fix(example): add more example

--- example/example.js
+++ example/example.js
@@ -1,7 +1,9 @@
 const scrapFavicon = require('..');
-
 // fetching favicon urls and its meta information like width, height, type, etc.
-scrapFavicon('https://akansh.com', { urlsOnly: false}).then(resp => console.log(resp), err => console.log(err));
+scrapFavicon('http://akansh.com').then(resp => console.log("resp", resp), err => console.log("E", err));
 
 // fetching only the urls of the favicons
-// scrapFavicon('https://akansh.com', { urlsOnly: true}).then(resp => console.log(resp), err => console.log(err));
+scrapFavicon('http://akansh.com', {urlsOnly: true}).then(resp => console.log("resp", resp), err => console.log("E", err));
+
+// trying to fetch from not a url, gives error
+scrapFavicon('http://a.com/').then(resp => console.log(resp), err => console.log("E", err));
